e5999d8f791d53f59acbc2cc27199c7f59620745
JayCRL <141310910+JayCRL@users.noreply.github.com>
2023-09-01 10:33:21 +0800

    写入奇偶分开排序
    思路：先冒泡整体排序然后按顺序输出一遍
    先奇数然后再来一遍的时候偶数
    注意：空格！！！ 要写个标识 比如count和flage等

--- C6020.c
+++ C6020.c
@@ -1,1 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS 1;
+#include <stdio.h>
+int main() {
+	int in[99999];
+	int n = 0;
+	int b, c, d;
+	int i, j, a;
+	scanf("%d", &n);//输入总个数
+	getchar();//清除缓存区
+	for (int o = 0; o < n; o++) {
+		scanf("%d", &in[o]);//读取的时候不用清除缓存区
+	}
+  //冒泡排序
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j + 1 <= n - i; j++) {
+			if (in[j] < in[j + 1]) {
+				a = in[j];
+				in[j] = in[j + 1];
+				in[j + 1] = a;
+			}
+		}
+	}
+  //输出奇数
+	for (int p = 0; p < n; p++) {
+		if (in[n - 1 - p] % 2 != 0) {
+			if (n - 1 - p == 0) {
+				printf("%d", in[n - 1 - p]);
+			}
+			else {
+				printf("%d ", in[n - 1 - p]);
+			}
 
+		}
+	}
+	printf(" ");//奇偶数直接一个空格分开
+  //输出偶数
+	for (int p = 0; p < n; p++) {
+		static int count = 0;
+		if (in[n - 1 - p] % 2 == 0) {
+			if (count == 0) {
+				printf("%d", in[n - 1 - p]);
+				count++;
+			}
+			else {
+				printf(" %d", in[n - 1 - p]);
+			}
+
+		}
+	}
+}
